4fe6acfd1915c2b1b8c30cb12000c8e854263430
hangforever <435189575@qq.com>
2018-08-01 19:38:16 +0800

    Update BFS(广度优先).cpp

--- Algorithm/BFS(广度优先).cpp
+++ Algorithm/BFS(广度优先).cpp
@@ -1,3 +1,17 @@
+/*
+ * 广度优先本质上使用队列进行计算。每次加入节点的都为当前处理节点的子节点（相关节点）。以层级的模式进行处理.
+ * 模版
+ * void BFS(* 需要进行计算的参数 *) {
+ *      queue<int> q;
+ *      q.push(* 将根节点入队 *);
+ *      while(!q.empty()) {         //当队列不为空时
+ *          //取出队首元素 top
+ *          //访问队首元素 top
+ *          //将队首元素出队
+ *          //将 top 的下一层级所有未入队的节点全部入队，并标记已入队
+ *      }
+ * }
+ */
 #include <iostream>
 #include <queue>
 
@@ -13,10 +27,10 @@ int n, m;                                           //矩阵大小
 int matrix[maxn][maxn];                             //01矩阵
 bool inq[maxn][maxn] = {false};                     //记录(x,y)是否入过队
 int X[4] = {0, 0, 1, -1};                           //增量数组
-int Y[4] = {1, -1, 0, 0};
+int Y[4] = {1, -1, 0, 0};                           //通过提取相同增量来简化工程
 
 bool judge(int x, int y) {                          //判断(x,y)是否需要访问
-    if (x >= n || x < 0 || y > m || y < 0)          //越界
+    if (x >= n || x < 0 || y > m || y < 0)          //越界!!!!
         return false;
     if (matrix[x][y] == 0 || inq[x][y] == true)     //在矩阵中为0 或者 被访问过
         return false;
